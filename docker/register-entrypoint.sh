aa59892dfb016abc2b49a84d896605c7dca2db50
Nikita Pivkin <nikita.pivkin@smartforce.io>
2023-03-16 10:41:35 +0600

    style: apply formatter

--- docker/register-entrypoint.sh
+++ docker/register-entrypoint.sh
@@ -5,17 +5,17 @@ CONF_FILE=/etc/gitlab-runner/config.toml
 echo -e "Starting registration script...\n"
 if [ ! -s "${CONF_FILE}" ]; then
     gitlab-runner register \
-    --non-interactive \
-    --url "http://gitlab" \
-    --registration-token "${REGISTRATION_TOKEN}" \
-    --executor "docker" \
-    --docker-image alpine:latest \
-    --docker-network-mode "gitlab_net" \
-    --description "docker-runner" \
-    --tag-list "docker" \
-    --run-untagged="true" \
-    --locked="false" \
-    --access-level="not_protected"
+        --non-interactive \
+        --url "http://gitlab" \
+        --registration-token "${REGISTRATION_TOKEN}" \
+        --executor "docker" \
+        --docker-image alpine:latest \
+        --docker-network-mode "gitlab_net" \
+        --description "docker-runner" \
+        --tag-list "docker" \
+        --run-untagged="true" \
+        --locked="false" \
+        --access-level="not_protected"
 else
     echo -e "Ignoring registration : config.toml file not empty (already registered)."
 fi
